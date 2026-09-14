136d2b49bd5e6fc74d2907608bd4af6af697ffe4
kompik <Ali221105.A@gmail.com>
2026-09-14 14:35:34 +0700

    DemoScreen

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -10,12 +10,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,4 +63,11 @@ fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit ) {
         value = sliderPosition,
         onValueChange = { onPositionChange(it) }
     )
+}
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+    var sliderPosition by remember { mutableFloatStateOf(20f) }
+    val handlePositionChange = { position : Float ->
+        sliderPosition = position
+    }
 }
